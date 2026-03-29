b16ebbb8d88c85e1f6cc6c294822aa83b55e81dd
Alex Ramirez <alexramirezroble@u.boisestate.edu>
2026-03-28 22:22:35 -0600

    Added Mock Data

---
+++ src/main/resources/database/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO trails (name, date, city, state, difficulty, distance, rating, notes) VALUES
+('Demo Trail 1', '2026-01-01', 'Boise', 'ID', 'Easy', 1.0, 1, 'This trail is a demo with EASY difficulty'),
+('Demo Trail 2', '2026-02-02', 'Boise', 'ID', 'Medium', 2.0, 2, 'This trail is a demo with MEDIUM difficulty'),
+('Demo Trail 3', '2026-03-02', 'Boise', 'ID', 'Hard', 3.0, 3, 'This trail is a demo with HARD difficulty');
