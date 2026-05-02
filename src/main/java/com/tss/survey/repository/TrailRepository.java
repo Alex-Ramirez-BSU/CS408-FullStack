33d762dfa1a748f1c7f35286c0df4f897f3c115a
Alex Ramirez <alexramirezroble@u.boisestate.edu>
2026-05-02 13:11:07 -0600

    Updated Documentation

--- src/main/java/com/tss/survey/repository/TrailRepository.java
+++ src/main/java/com/tss/survey/repository/TrailRepository.java
@@ -3,5 +3,11 @@ package com.tss.survey.repository;
 import com.tss.survey.model.Trail;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * Repository interface for Trail entities.
+ *
+ * Extends JpaRepository to provide built-in CRUD operations
+ * such as save, findAll, findById, delete, etc.
+ */
 public interface TrailRepository extends JpaRepository<Trail, Integer> {
 }
